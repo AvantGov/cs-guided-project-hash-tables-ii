a823a25de79479642edbe8b6ba87ae6d1a48081f
avantgov <alexcouts18@gmail.com>
2020-11-12 20:37:32 -0600

    implemented solutions

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -42,14 +42,63 @@ Notes:
 - words in the input list only contain lowercase letters.
 ```
 """
+"""
+Input:
+words -> List[str]
+k -> int
+
+Output:
+List[str]
+"""
+
+"""
+UNDERSTAND: 
+- given a list of words, return the most frequent elements in the sentence
+- return a list of the strings that are most common in the given list 
+
+- k will serve as the collector for the most common words, so that any items
+that have k amount of words will be included in the list of words 
+
+PLAN 
+Option 1: implement with list.count()
+- list.count(x) will return the number of times x appears in the list 
+    - this would require us to iterate through the list each time for each different item
+
+Option 2:
+- use a dictionary to store the words as keys and number of appearances as values 
+    - iterate through the words 
+    - if the word is already in the dictionary, incremement the count and move closer 
+    - if it is not, add it to the dictionary and set it to a count of 1
+
+Option 3: 
+- iterate over the values and return the leys for the top 4 items (alphabetically for ties)
+    - this could be done as a heap 
+    - we could also sort the list of words 
+    - get max() k times and remove from the hash table
+
+"""
+
+
+"""
+IMPLEMENTATION CHOICE:
+Option 2:
+- use a dictionary to store the words as keys and number of appearances as values 
+    - iterate through the words 
+    - if the word is already in the dictionary, incremement the count and move closer 
+    - if it is not, add it to the dictionary and set it to a count of 1
+"""
 def top_k_frequent(words, k):
-    """
-    Input:
-    words -> List[str]
-    k -> int
+    ref_dict = {}
+    for word in words:
+        if word in ref_dict:
+            ref_dict[word] += 1
+        else:
+            ref_dict[word] = 1
+    print(ref_dict)
 
-    Output:
-    List[str]
-    """
-    # Your code here
+# reverse sort the list of keys to be in descending order 
+    sorted_keys = sorted(ref_dict, key=ref_dict.get, reverse=True)
+    print(sorted_keys)
 
+# returning the top k items
+    return sorted_keys[:k]
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -42,15 +42,65 @@ Explanation:
 "ccbB" is also a valid answer, but "Bbcc" is incorrect.
 Note that 'B' and 'b' are treated as two different characters.
 ```
+
+Inputs:
+s -> str
+
+Output:
+str
+
 """
-def frequency_sort(s: str) -> str:
-    """
-    Inputs:
-    s -> str
+"""
+UNDERSTAND: 
+- given a string of letters, sort the string in desc order based on the amount of appearances 
+    - there will be capital and lowercase items in the string, they should be treated differently
+- return the items in decreasing order of appearances in the string 
 
-    Output:
-    str
-    """
-    # Your code here
+** since the capital and lowercase items will be recognized as different upon comparison, we do not 
+need to perform any kind of manipulation on the items 
 
+PLAN:
+- use a dictionary to store the different letters that appear in the string
+    - iterate through the string, adding the different letters to the dict / iterating the count of items 
+
+- sort the keys in a desc order 
+
+- create collector string 
+- iterate through the keys of the dictionary 
+    for items in range 1 > num of appearances 
+        append a copy of the key to the collector string 
+    return collector string 
+
+"""
+# this is known as a type-note that just lets devs know the type of expected data input coming in
+# this has no syntatic meaning 
+# def frequency_sort(s: str) -> str:
+
+def frequency_sort(string):
+    dict__occurances = {}
+
+    for letter in string:
+        if letter in dict__occurances:
+            dict__occurances[letter] += 1
+        else:
+            dict__occurances[letter] = 1
+    print(dict__occurances)
+
+
+    sorted_keys = sorted(dict__occurances, key = dict__occurances.get, reverse=True)
+    print("sorted items:", sorted_keys)
+
+    collector = ''
+
+    for key in sorted_keys:
+        count = dict__occurances[key]
+        for i in range(count):
+            collector += key
+        print("new string:", collector)
+    
+    return collector
+
+
+
+frequency_sort('free')
     
